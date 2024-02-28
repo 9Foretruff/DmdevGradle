f87e89a828f1bc14c7940af65c9a0014ad6b7bb8
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-28 23:56:03 +0200

    lesson 23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import org.springframework.util.StringUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(StringUtils.capitalize("hello world"));
     }
 }
